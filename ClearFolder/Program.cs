defc52d7e7bb9789399710e18af5485cf8efd426
unknown <mammutipoja1@gmail.com>
2020-11-19 17:07:42 +0200

    ClearFolder

---
+++ ClearFolder/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+
+namespace ClearFolder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootpath = @"C:\Users\mammu\samples";
+            DeleteAllfiles();
+            DirectoryInfo rootDirectory = new DirectoryInfo(rootpath);
+            foreach (DirectoryInfo dir in rootDirectory.GetDirectories())
+           
+            {
+                 DeleteAllfolders(dir.FullName, true);
+            }
+
+
+        }
+
+
+        public static void DeleteAllfiles()
+        {
+            string rootpath = @"C:\Users\mammu\samples";
+            DirectoryInfo directory = new DirectoryInfo(rootpath);
+            foreach (FileInfo file in directory.GetFiles())
+            {
+                file.Delete();
+                
+            }
+
+
+
+        }
+        public static void DeleteAllfolders(string directoryName, bool ifExists)
+        {
+            if (Directory.Exists(directoryName))
+            {
+                Directory.Delete(directoryName, true);
+
+            }else if (ifExists)
+            {
+                throw new SystemException("No such directory to delete");
+            }
+
+        }
+
+
+
+    }
+
+
+
+}
